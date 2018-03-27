82ac3589e3ee9ccad5973414df3631f4015faa93
Angus Williams <aw16797@my.bristol.ac.uk>
2018-03-27 10:52:49 +0100

    just p3 and p4

--- kernel/hilevel.c
+++ kernel/hilevel.c
@@ -6,10 +6,9 @@
  */
 
 #include "hilevel.h"
-int processes = 2:
-int n = processes - 1;
+int n = 1;
 int executing = 0;
-pcb_t pcb[ processes ];
+pcb_t pcb[ 2 ];
 int current = 0;
 int new = 0;
 
@@ -71,12 +70,12 @@ void hilevel_handler_rst(ctx_t* ctx) {
   pcb[ 1 ].ctx.pc   = ( uint32_t )( &main_P4 );
   pcb[ 1 ].ctx.sp   = ( uint32_t )( &tos_P4  );
 
-  // memset( &pcb[ 2 ], 0, sizeof( pcb_t ) );
-  // pcb[ 2 ].pid      = 3;
-  // pcb[ 2 ].status   = STATUS_READY;
-  // pcb[ 2 ].ctx.cpsr = 0x50;
-  // pcb[ 2 ].ctx.pc   = ( uint32_t )( &main_P5 );
-  // pcb[ 2 ].ctx.sp   = ( uint32_t )( &tos_P5  );
+  memset( &pcb[ 2 ], 0, sizeof( pcb_t ) );
+  pcb[ 2 ].pid      = 3;
+  pcb[ 2 ].status   = STATUS_READY;
+  pcb[ 2 ].ctx.cpsr = 0x50;
+  pcb[ 2 ].ctx.pc   = ( uint32_t )( &main_P5 );
+  pcb[ 2 ].ctx.sp   = ( uint32_t )( &tos_P5  );
 
   memcpy( ctx, &pcb[ 0 ].ctx, sizeof( ctx_t ) );
   pcb[ 0 ].status = STATUS_EXECUTING;
