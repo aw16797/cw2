3bfa818dec8e7e65a9d03a195b75dd3bb08dad52
Angus Williams <aw16797@my.bristol.ac.uk>
2018-04-17 23:17:27 +0100

    include 1b code

--- kernel/hilevel.c
+++ kernel/hilevel.c
@@ -115,10 +115,6 @@ void updatePriority(){
 }
 
 void scheduler( ctx_t* ctx ) {
-  //update nid with max prtc process
-  //nid = findMaxPriority();
-  // nid = matchCTX(ctx);
-  // if (nid == -1) PL011_putc( UART0, 'Q', true );
   PL011_putc( UART0, ' ', true );
   PL011_putc( UART0, 'S', true );
   PL011_putc( UART0, ' ', true );
@@ -135,9 +131,6 @@ void scheduler( ctx_t* ctx ) {
 
     cid = nid;
   }
-
-  //updatePriority();
-
   return;
 }
 
@@ -262,11 +255,6 @@ void hilevel_handler_svc(ctx_t* ctx, uint32_t id) {
       pcb[ newpcb ].ctx.pc   = ( uint32_t )( ctx->pc );
       pcb[ newpcb ].ctx.sp   = ( uint32_t )( newtos );
 
-
-
-      // memcpy( ctx, &pcb[ newpcb ].ctx, sizeof( ctx_t ) );
-      // pcb[ newpcb ].status = STATUS_EXECUTING;
-
       // if(newpcb < 10){ //if space for new processes
       //   newpcb++;
       // } else{
@@ -295,20 +283,6 @@ void hilevel_handler_svc(ctx_t* ctx, uint32_t id) {
 
       insert(newpcb);
 
-      //nid = matchCTX(ctx);
-      // if(nid == -1){
-      //   PL011_putc( UART0, 'Y', true );
-      // }
-      // //preserve
-      // memcpy( &pcb[ cid ].ctx, ctx, sizeof( ctx_t ) );
-      // pcb[ cid ].status = STATUS_READY;
-      //
-      // //restore
-      // memcpy( ctx, &pcb[ nid ].ctx, sizeof( ctx_t ) );
-      // pcb[ nid ].status = STATUS_EXECUTING;
-      //
-      // cid = nid;
-
       break;
     }
     case 0x06 : { //0x06 => kill( pid, x ),
@@ -321,3 +295,164 @@ void hilevel_handler_svc(ctx_t* ctx, uint32_t id) {
   }
   return;
 }
+// --------------------------------------------------
+// CODE FOR 1B (PRIORITY)
+
+// #include "hilevel.h"
+// #define pnum 2
+// #define ni (pnum-1)
+// pcb_t pcb[ pnum ];
+// int cid = 1;
+// int nid = 1;
+// int current = 0;
+// int next = 0;
+//
+// extern void     main_P3();
+// extern uint32_t tos_P3;
+// extern void     main_P4();
+// extern uint32_t tos_P4;
+//
+// // extern void     main_P5();
+// // extern uint32_t tos_P5;
+//
+// pid_t findMaxPriority(){
+//   int maxP = pcb[0].prtc;
+//   pid_t maxPi = pcb[0].pid;
+//   for (int i = 1; i < pnum ; i++){
+//     if (pcb[i].prtc > maxP){
+//       maxP = pcb[i].prtc;
+//       maxPi = pcb[i].pid;
+//     }
+//   }
+//   return maxPi;
+// }
+//
+// void updatePriority(){
+//   for (int i = 0; i < pnum; i++){
+//     if (pcb[i].pid != cid){
+//       pcb[i].prtc = pcb[i].prtc + pcb[i].prtb;
+//     }
+//   }
+// }
+//
+// void scheduler( ctx_t* ctx ) {
+//   //find max p process
+//   //preserve old, restore new
+//   //increment other processes p
+//
+//   nid = findMaxPriority();
+//   next = nid-1;
+//
+//   //preserve
+//   memcpy( &pcb[ current ].ctx, ctx, sizeof( ctx_t ) );
+//   pcb[ current ].status = STATUS_READY;
+//
+//   //restore
+//   memcpy( ctx, &pcb[ next ].ctx, sizeof( ctx_t ) );
+//   pcb[ next ].status = STATUS_EXECUTING;
+//
+//   updatePriority();
+//
+//   //update current index
+//   cid = nid;
+//   current = next;
+//
+//   return;
+// }
+//
+// void hilevel_handler_rst(ctx_t* ctx) {
+//
+//   TIMER0->Timer1Load  = 0x00100000; // select period = 2^20 ticks ~= 1 sec
+//   TIMER0->Timer1Ctrl  = 0x00000002; // select 32-bit   timer
+//   TIMER0->Timer1Ctrl |= 0x00000040; // select periodic timer
+//   TIMER0->Timer1Ctrl |= 0x00000020; // enable          timer interrupt
+//   TIMER0->Timer1Ctrl |= 0x00000080; // enable          timer
+//
+//   GICC0->PMR          = 0x000000F0; // unmask all            interrupts
+//   GICD0->ISENABLER1  |= 0x00000010; // enable timer          interrupt   pcb[ (current + 1) ].status = STATUS_EXECUTING;
+//   GICC0->CTLR         = 0x00000001; // enable GIC interface
+//   GICD0->CTLR         = 0x00000001; // enable GIC distributor
+//
+//   memset( &pcb[ 0 ], 0, sizeof( pcb_t ) );
+//   pcb[ 0 ].pid      = 1;
+//   pcb[ 0 ].status   = STATUS_READY;
+//   pcb[ 0 ].ctx.cpsr = 0x50;
+//   pcb[ 0 ].ctx.pc   = ( uint32_t )( &main_P3 );
+//   pcb[ 0 ].ctx.sp   = ( uint32_t )( &tos_P3  );
+//   pcb[ 0 ].prtb     = 3;
+//   pcb[ 0 ].prtc     = 0;
+//
+//   memset( &pcb[ 1 ], 0, sizeof( pcb_t ) );
+//   pcb[ 1 ].pid      = 2;
+//   pcb[ 1 ].status   = STATUS_READY;
+//   pcb[ 1 ].ctx.cpsr = 0x50;
+//   pcb[ 1 ].ctx.pc   = ( uint32_t )( &main_P4 );
+//   pcb[ 1 ].ctx.sp   = ( uint32_t )( &tos_P4  );
+//   pcb[ 1 ].prtb     = 1;
+//   pcb[ 1 ].prtc     = 0;
+//
+//   // memset( &pcb[ 2 ], 0, sizeof( pcb_t ) );
+//   // pcb[ 2 ].pid      = 3;
+//   // pcb[ 2 ].status   = STATUS_READY;
+//   // pcb[ 2 ].ctx.cpsr = 0x50;
+//   // pcb[ 2 ].ctx.pc   = ( uint32_t )( &main_P5 );
+//   // pcb[ 2 ].ctx.sp   = ( uint32_t )( &tos_P5  );
+//
+//   memcpy( ctx, &pcb[ 0 ].ctx, sizeof( ctx_t ) );
+//   pcb[ 0 ].status = STATUS_EXECUTING;
+//
+//   cid = 1;
+//   nid = 1;
+//   current = 0;
+//   next = 0;
+//
+//   int_enable_irq();
+//
+//   return;
+// }
+//
+// void hilevel_handler_irq(ctx_t* ctx) {
+//   uint32_t id = GICC0->IAR;
+//
+//   if( id == GIC_SOURCE_TIMER0 ) {
+//     scheduler( ctx ); TIMER0->Timer1IntClr = 0x01;
+//   }
+//
+//   GICC0->EOIR = id;
+//
+//   return;
+// }
+//
+// void hilevel_handler_svc(ctx_t* ctx, uint32_t id) {
+//
+//   switch( id ) {
+//     case 0x00 : { // 0x00 => yield()
+//       scheduler( ctx );
+//       break;
+//     }
+//
+//     case 0x01 : { // 0x01 => write( fd, x, n )
+//       int   fd = ( int   )( ctx->gpr[ 0 ] );
+//       char*  x = ( char* )( ctx->gpr[ 1 ] );
+//       int    n = ( int   )( ctx->gpr[ 2 ] );
+//
+//       for( int i = 0; i < n; i++ ) {
+//         PL011_putc( UART0, *x++, true );
+//       }
+//
+//       ctx->gpr[ 0 ] = n;
+//       break;
+//     }
+//     case 0x04 : {//0x04 => exit
+//       //clean pcb for p5
+//
+//       //call scheduler
+//     }
+//
+//     default   : { // 0x?? => unknown/unsupported
+//       break;
+//     }
+//   }
+//   return;
+// }
+//
