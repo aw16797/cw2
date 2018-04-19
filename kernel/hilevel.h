a271d892d3939c4d84394fecc57d5055720b642c
Angus Williams <aw16797@my.bristol.ac.uk>
2018-04-19 23:12:25 +0100

    scheduler in exit

--- kernel/hilevel.h
+++ kernel/hilevel.h
@@ -13,9 +13,9 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
+// #include <string.h>
+// #include <stdio.h>
+// #include <stdlib.h>
 
 // Include functionality relating to the   kernel.
 
